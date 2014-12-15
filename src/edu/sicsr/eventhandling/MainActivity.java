349b9615363c1d67e2174def178a04c663a60615
Adhirath03 <adhirathssharma@gmail.com>
2014-12-15 09:47:25 +0530

    message

--- src/edu/sicsr/eventhandling/MainActivity.java
+++ src/edu/sicsr/eventhandling/MainActivity.java
@@ -1,22 +1,30 @@
 package edu.sicsr.eventhandling;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+private Button btnclick;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	btnclick = (Button) findViewById(R.id.btnclick);
+	OnClickListener listener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			btnclick.setText("button was clicked");
+			
+			
+		}
+	};	
+	btnclick.setOnClickListener(listener);
+	
 	}
 
 }
