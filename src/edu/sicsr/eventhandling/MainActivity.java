b7bdd9bdcc103d62a7f742bb458c9e2a95d57551
Adhirath03 <adhirathssharma@gmail.com>
2014-12-19 11:08:48 +0530

    project

--- src/edu/sicsr/eventhandling/MainActivity.java
+++ src/edu/sicsr/eventhandling/MainActivity.java
@@ -5,26 +5,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-private Button btnclick;
+private Button click;
+private EditText name;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	
-	btnclick = (Button) findViewById(R.id.btnclick);
+	click = (Button) findViewById(R.id.btnclick);
+	name= (EditText) findViewById(R.id.editText);
 	OnClickListener listener = new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
-			btnclick.setText("button was clicked");
+			Toast.makeText(getApplicationContext(), name.getText(),Toast.LENGTH_LONG).show();
 			
 			
 		}
 	};	
-	btnclick.setOnClickListener(listener);
-	
+	click.setOnClickListener(listener);
 	}
 
 }
